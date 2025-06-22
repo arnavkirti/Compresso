a0fbe40393ceb13d5602aefe056d2561ef7d652b
Arnav Kirti <arnav.tkd@gmail.com>
2025-06-22 17:33:45 +0530

    fixed and deployed

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -6,7 +6,7 @@ import type {
   AlgorithmInfo
 } from '../types/compression.types';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://compresso-rku9.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
