e32609d2b36a296522a2d5eda1e2407a54f0573c
Arnav Kirti <arnav.tkd@gmail.com>
2025-06-22 17:35:14 +0530

    fixed and deployed

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', ['https://compresso-psi.vercel.app/']],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
